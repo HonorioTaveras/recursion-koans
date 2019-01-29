00730960d5e957ec1b1046f73d173e5ae2f7c423
JohnMichelin <jmichelinphone@gmail.com>
2019-01-28 22:05:07 -0800

    fix(hrext07): clear for new cohort

--- functions/distance-to-bun.js
+++
@@ -1,7 +0,0 @@
-var distanceToBun = function(ingredients, ingredient) {
-  for(var i = 0; i < ingredients.length; i++) {
-    if(ingredients[i] === ingredient) {
-      return Math.min(i, ingredients.length - i);
-    }
-  }
-}
